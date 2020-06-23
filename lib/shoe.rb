9881573f0d4357c7cb5fb45e1caac41647097942
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-23 21:35:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,16 @@ class Shoe
 BRANDS = []
   def initialize(brand)
     @brand = brand
+      BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-def brand=(brand)
-  @brand=brand
-  BRANDS << brand
+# def brand=(brand)
+#   @brand=brand
+
 # brand.length
 end
 end
